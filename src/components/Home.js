235afb3ffed894211fa430430e464d71889a74b0
tyler martin <t.mar92@gmail.com>
2019-12-14 16:58:35 -0500

    adds navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import '../styles/home.scss'
+import Navbar from './Navbar'; 
+import '../styles/home.scss';
+import ContentList from './ContentList';
 
 export default function() {
   return (
     <div className="container">
+      <Navbar />
       <h1>Welcome Home</h1>
+      <ContentList />
     </div>
   )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/navbar.scss'
+
+export default function() {
+  return (
+    <nav>
+      <ul>
+        <li>Contact</li>
+        <li>Projects</li>
+        <li>Posts</li>
+      </ul>
+    </nav>
+  )
+}
