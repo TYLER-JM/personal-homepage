803384981370db607747deaf06c345fa34efd57f
tyler martin <t.mar92@gmail.com>
2019-12-19 12:09:12 -0500

    adds more divs to create a chevron/arrow for opening and closing articles

--- src/components/ArticleListItem.js
+++ src/components/ArticleListItem.js
@@ -11,10 +11,15 @@ const ArticleListItem = function() {
   return (
     <article
       className={classNames({"selected-article": selected})}
-      onClick={() => handleClick()}
     >
-      <h2>title</h2>
-      <p className="preview-text">this is thumbnail text...</p>
+      <div className="article-header">
+        <h2>title</h2>
+        <div className="arrow open" onClick={() => handleClick()}>
+          <div className="left"></div>
+          <div className="right"></div>
+        </div>
+        <p className="preview-text">this is thumbnail text...</p>
+      </div>
       <p className="article-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </article>
   )
