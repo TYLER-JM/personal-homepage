5daab2f4de688cdcf63b4ec3adcce8b0b2bac2fa
tyler martin <t.mar92@gmail.com>
2019-12-19 11:25:55 -0500

    adds placeholder contents and preview text to article

--- src/components/ArticleListItem.js
+++ src/components/ArticleListItem.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/articles.scss';
+import classNames from 'classnames';
 
 const ArticleListItem = function() {
+  const [selected, setSelected] = useState(false);
+  const handleClick = () => {
+    selected ? setSelected(false) : setSelected(true);
+    
+  }
   return (
-    <article>click me</article>
+    <article
+      className={classNames({"selected-article": selected})}
+      onClick={() => handleClick()}
+    >
+      <h2>title</h2>
+      <p className="preview-text">this is thumbnail text...</p>
+      <p className="article-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </article>
   )
 }
 
