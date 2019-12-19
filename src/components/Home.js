536e8ece308362ea0e8c3da52db0d4a8f34531fa
tyler martin <t.mar92@gmail.com>
2019-12-19 10:31:09 -0500

    renders new Articles component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Navbar from './Navbar'; 
+import Themes from './Navbar'; 
 import '../styles/home.scss';
 import ContentList from './ContentList';
+import ArticleListItem from './ArticleListItem';
 
 export default function() {
   return (
     <div className="container">
-      <Navbar />
-      <h1>Welcome Home</h1>
+      <Themes />
       <ContentList />
+      <ArticleListItem />
     </div>
   )
 }
