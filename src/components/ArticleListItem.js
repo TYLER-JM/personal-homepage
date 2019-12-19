f30159be4111a54a6b97fcf7ccfb129168bfd539
tyler martin <t.mar92@gmail.com>
2019-12-19 10:30:06 -0500

    adds component to hold posts

---
+++ src/components/ArticleListItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/articles.scss';
+
+const ArticleListItem = function() {
+  return (
+    <article>click me</article>
+  )
+}
+
+export default ArticleListItem;
