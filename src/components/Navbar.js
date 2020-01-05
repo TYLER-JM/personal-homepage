a4d7bad2d71c2d58411682306ba1c4351347e36d
tyler martin <t.mar92@gmail.com>
2020-01-04 21:50:02 -0500

    moves style rules to component file using @material-ui makeStyles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import '../styles/navbar.scss'
+import '../styles/navbar.scss';
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  menu: {
+    display: "flex",
+    "flex-direction": "column",
+    "justify-content": "center",
+    "align-items": "center",
+    background: "#303641",
+    opacity: "0.85",
+    height: "200px"
+  },
+  hero: {
+    "border-radius": "100%",
+    height: "100px",
+    width: "100px"
+  }
+})
 
 const Navbar = function() {
+  const classes = useStyles();
   return (
-    <nav className="themes">
-      <img src="images/generic-coding.jpg" alt="generic depiction of coding"></img>
+    // <nav className="themes">
+    <nav className={classes.menu}>
+      <img className={classes.hero} src="images/generic-coding.jpg" alt="generic depiction of coding"></img>
       <ul>
         <li>About</li>
         <li>Contact</li>
