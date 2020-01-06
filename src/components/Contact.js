2ba4d8fa9b2bc015693eb27abbb5957aca9812f4
tyler martin <t.mar92@gmail.com>
2020-01-06 11:21:22 -0500

    adds info in contact secion

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,10 @@ import '../styles/section.scss';
 const Contact = function() {
   return (
     <article className="section contact">
-      Contact section
+      <p>Call Me: <span>555.555.5555</span></p>
+      <p>Email Me: <span>email.email.ca</span></p>
+      <p>Github: <span><i class="fab fa-github"></i></span></p>
+      <p>LinkedIn: <span>logo</span></p>
     </article>
   )
 }
