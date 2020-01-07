fa98272830b77509b2d24c244fb5f13bf81b0f07
tyler martin <t.mar92@gmail.com>
2020-01-07 11:39:47 -0500

    styling and positioning the cards, making them scroll in their container

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import '../../styles/projectCard.scss';
 
-const ProjectCard = function () {
+const ProjectCard = function ({ title, tech, desc, href}) {
   return (
-    <ul className="cardItem">
-      <li className="card">
-        <a href="https://github.com/TYLER-JM/stuvv-react-frontend" target="blank">
+      <a href={href} className="card-link" target="blank">
+        <div className="card">
           <div className="card__flipper">
             <div className="card__front">
-              <p className="card__title">Stuvv</p>
-              <p className="card__tech"><span>React, Ruby on Rails, PostgreSQL</span></p>
-              <p className="card__name"><span>Rent out your stuff, why not?</span></p>
+              <p className="card__title">{title}</p>
+              <p className="card__tech"><span>{tech}</span></p>
+              <p className="card__name"><span>{desc}</span></p>
             </div>
-            {/* <div className="card__back">
-              <svg height="180" width="180">
-                <circle cx="90" cy="90" r="55" stroke="#514d9b" stroke-width="35" />
-              </svg>
-              <span>113.2</span>
-            </div> */}
           </div>
-        </a>
-      </li>
-    </ul>
+        </div>
+      </a>
   )
 
 }
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,9 +2,57 @@ import React from 'react';
 import ProjectCard from './ProjectCard';
 
 const Projects = function() {
+  const projects = [
+    {
+      title: "Stuvv",
+      tech: "React, Ruby on Rails, PostgreSQL",
+      desc: "Rent out your stuff, why not?",
+      href: "https://github.com/TYLER-JM/stuvv-react-frontend"
+    },
+    {
+      title: "Scheduler",
+      tech: "React, Storybook, Jest, Cypress",
+      desc: "Schedule interview appointments",
+      href: "https://github.com/TYLER-JM/scheduler"
+    },
+    {
+      title: "Toodleedoo",
+      tech: "Express, EJS, PostgreSQL, Sass, Bootstrap, Google APIs",
+      desc: "A 'smart' To-Do List, leveraging third-party APIs to sort tasks",
+      href: "https://github.com/TYLER-JM/toodleedoo"
+    },
+    {
+      title: "Tweeter",
+      tech: "Express, AJAX, jQuery, CSS",
+      desc: "A twitter clone, focusing on front-end, responsive design",
+      href: "https://github.com/TYLER-JM/tweeter"
+    },
+    {
+      title: "Bar Chart",
+      tech: "jQuery, HTML, CSS",
+      desc: "An API to generate Bar charts on an web page",
+      href: "https://github.com/TYLER-JM/barChart_repo"
+    }
+  ]
+
+  const allProjectCards = projects.map(project => {
+    return (
+      <ProjectCard
+        title={project.title}
+        tech={project.tech}
+        desc={project.desc}
+        href={project.href}
+      />
+    )
+  })
+
   return (
     <article className="section projects" id="projects">
-      <ProjectCard />
+      <div className="scroll-me">
+        {/* <ul className="card-list"> */}
+          {allProjectCards}
+        {/* </ul> */}
+      </div>
     </article>
   );
 }
