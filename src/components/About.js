5631ddbc8529dff9a3720aa3a565c8f2af498710
tyler martin <t.mar92@gmail.com>
2020-01-14 19:21:59 -0500

    adds new fonts throughout page

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = function() {
   return (
     <article className="section about" id="about">
-      <p className="bio">Welcome, I am a full-stack developer, and a Robertson Davies aficionado. I have a perfectly healthy love for board and card games</p>
+      <p className="bio">Welcome, I am a full-stack developer, and a Robertson Davies aficionado. I have a perfectly healthy love for board and card games.</p>
       <h1 className="name">Tyler Martin</h1>
       {/* <p>With bended neck I grope, </p> */}
       </article>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import '../styles/section.scss';
 const Contact = function() {
   return (
     <article className="section contact" id="contact">
-      <h3>Find out more about me and my talents...</h3>
+      <h3>Check back soon, and watch the evolution.</h3>
       <p><img src="images/github-48.png" alt="GitHub logo"/></p>
       <p><img src="images/linkedin-50.png" alt="Linked in logo"/></p>
     </article>
