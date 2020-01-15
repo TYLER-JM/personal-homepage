21f3d81e2e61a8c82f8d336e0021f71b3e96df1a
tyler martin <t.mar92@gmail.com>
2020-01-15 17:08:15 -0500

    work on creating a better animation

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,9 @@ const About = function() {
   return (
     <article className="section about" id="about">
       <p className="bio">Welcome, I am a full-stack developer, and a Robertson Davies aficionado. I have a perfectly healthy love for board and card games.</p>
-      <h1 className="name">Tyler Martin</h1>
+      {/* <h1 className="name">Tyler Martin</h1> */}
+      <h1 className="name first">Tyler</h1>
+      <h1 className="name last">Martin</h1>
       {/* <p>With bended neck I grope, </p> */}
       </article>
   );
