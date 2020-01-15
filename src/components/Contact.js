c7ff2c3e6148448d61ac1faa816fac72369b1380
tyler martin <t.mar92@gmail.com>
2020-01-15 10:17:49 -0500

    adds a tag to contact logos

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,16 @@ const Contact = function() {
   return (
     <article className="section contact" id="contact">
       <h3>Check back soon, and watch the evolution.</h3>
-      <p><img src="images/github-48.png" alt="GitHub logo"/></p>
-      <p><img src="images/linkedin-50.png" alt="Linked in logo"/></p>
+      <p>
+        <a href="https://github.com/TYLER-JM" target="blank">
+          <img src="images/github-48.png" alt="GitHub logo"/>
+        </a>
+      </p>
+      <p>
+        <a href="https://www.linkedin.com/in/tyler-martin-670020198/" target="blank">
+          <img src="images/linkedin-50.png" alt="Linked in logo"/>
+        </a>
+      </p>
     </article>
   )
 }
