0d0786d2c7b0469e01212725a5c4aeaf58a8c5e3
tyler martin <t.mar92@gmail.com>
2020-01-15 17:45:58 -0500

    adds classes to work with react-transition-group

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import generateID from '../Helpers/generateId';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 const About = function() {
   return (
     <article className="section about" id="about">
       <p className="bio">Welcome, I am a full-stack developer, and a Robertson Davies aficionado. I have a perfectly healthy love for board and card games.</p>
-      {/* <h1 className="name">Tyler Martin</h1> */}
-      <h1 className="name first">Tyler</h1>
-      <h1 className="name last">Martin</h1>
-      {/* <p>With bended neck I grope, </p> */}
+      <CSSTransitionGroup
+        transitionName="slide"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnter={false}
+        transitionLeave={false}>
+        <h1 className="name" key={generateID(8)}>Tyler</h1>
+        <h1 className="name" key={generateID(8)}>Martin</h1>
+      </CSSTransitionGroup>
+      {/* <h1 className="name first">Tyler</h1>
+      <h1 className="name last">Martin</h1> */}
       </article>
   );
 }
