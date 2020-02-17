9cc22a1bbb88fd13cde6e1f3df68d506f803a016
tyler martin <t.mar92@gmail.com>
2020-02-17 10:58:32 -0500

    changes title of posts section, changes font

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Posts = function() {
   return (
     <article className="section posts" id="posts">
-      <p className="title">I have also written a few posts, if you're interested</p>
+      <h2 className="title">Posts</h2>
       <a href="https://medium.com/@rufustheblog/a-beginners-guide-to-web-frameworks-a131b625ee95">
         <div className="post-card">
           <div>
             <img src="../../images/mvc.png" alt="web frame work"/>
           </div>
-          <p>Web Frameworks and MVC architecture</p>
+            <p>Web Frameworks and MVC architecture</p>
         </div>
       </a>
       <a href="https://medium.com/@rufustheblog/so-you-want-to-use-an-orm-with-javascript-3895f9d437e6">
