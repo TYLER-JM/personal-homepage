a4fd9fb893ac9e7012387bca8dd7193cbf58f3f9
tyler martin <t.mar92@gmail.com>
2020-03-13 20:19:33 -0300

    improves About layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar'; 
-import Backdrop from './components/Backdrop';
+// import Backdrop from './components/Backdrop';
 import Contact from './components/Contact';
 import Projects from './components/projects/Projects';
 import About from './components/About';
--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import generateID from '../Helpers/generateId';
-import { CSSTransitionGroup } from 'react-transition-group';
 
 const About = function() {
 
@@ -12,20 +11,14 @@ const About = function() {
 
   return (
     <article className="section about" id="about">
-
-      <p className="bio">Welcome, I am a full-stack developer, and a Robertson Davies aficionado. I have a perfectly healthy love for board and card games.</p>
-      <CSSTransitionGroup
-        transitionName="slide"
-        transitionAppear={true}
-        transitionAppearTimeout={500}
-        transitionEnter={false}
-        transitionLeave={false}>
+      <div className="about__container">
         <h1 className="name" key={generateID(8)}>Tyler</h1>
         <h1 className="name" key={generateID(8)}>Martin</h1>
-      </CSSTransitionGroup>
-      {/* <h1 className="name first">Tyler</h1>
-      <h1 className="name last">Martin</h1> */}
-      </article>
+        <p className="bio">
+        Hello visitor. I am a full-stack developer, and a Robertson Davies aficionado. I have a perfectly healthy love for board and card games.
+        </p>
+      </div>
+    </article>
   );
 }
 
