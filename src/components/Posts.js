50b37b242facf43947830d1701560d4dbe4261bd
tyler martin <t.mar92@gmail.com>
2020-03-13 13:31:55 -0300

    removes window.innerWidth check

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -16,12 +16,11 @@ const Posts = function() {
             <img src="../../images/mvc.png" alt="web frame work"/>
           </div>
             <p className="post-card__title">Web Frameworks and MVC architecture</p>
-            {
-              window.innerWidth >= 428 && 
+
               <p className="post-card__description">
                 Object-relational mapping (ORM), as the name implies, is a way for an object-oriented programming language…
               </p>
-            }
+            
         </div>
       </a>
       <a href="https://medium.com/@rufustheblog/so-you-want-to-use-an-orm-with-javascript-3895f9d437e6">
@@ -30,12 +29,11 @@ const Posts = function() {
             <img src="../../images/sequelizejs.png" alt="Sequelize logo"/>
           </div>
           <p className="post-card__title">Learning the Sequelize ORM for Javascript</p>
-          {
-            window.innerWidth >= 428 &&
+         
             <p className="post-card__description">
               Lets learn about Web Frameworks. Before we can get into the details of individual frameworks, we need to understand…
             </p>
-          }
+          
         </div>
       </a>
     </article>
