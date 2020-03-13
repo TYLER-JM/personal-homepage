904a625c19f060c645b4cfccc604fb88fd4b4800
tyler martin <t.mar92@gmail.com>
2020-03-13 12:33:20 -0300

    moves code from Home component into App.js and deletes Home.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar'; 
+import Backdrop from './components/Backdrop';
+import Contact from './components/Contact';
+import Projects from './components/projects/Projects';
+import About from './components/About';
+import Posts from './components/Posts';
+import './styles/home.scss';
+import Skills from './components/Skills';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Backdrop />
+      <Navbar />
+      <About />
+      <Projects />
+      <Posts />
+      <Skills />
+      <Contact />
     </div>
-  );
+  )
 }
 
 export default App;
