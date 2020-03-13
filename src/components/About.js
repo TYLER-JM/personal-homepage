2af507cb94de9977d141950915e44e8178af26db
tyler martin <t.mar92@gmail.com>
2020-03-13 20:58:28 -0300

    adds fadein animation to section class

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 import generateID from '../Helpers/generateId';
+import '../styles/about.scss';
 
 const About = function() {
 
+  let classes = 'section about'
   useEffect(() => {
     return () => {
       console.log('im affecting change');
@@ -10,7 +12,7 @@ const About = function() {
   }, [])
 
   return (
-    <article className="section about" id="about">
+    <article className={classes} id="about">
       <div className="about__container">
         <h1 className="name" key={generateID(8)}>Tyler</h1>
         <h1 className="name" key={generateID(8)}>Martin</h1>
