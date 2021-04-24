dcda872c10c6ba41141a8403761253ca547b119b
Tyler Martin <tyler.martin@electric-playground.com>
2021-04-24 12:51:15 -0300

    moves img between name divs

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,8 @@ const About = function() {
     <article className={classes} id="about">
       <div className="about__container">
         <div className="hero">
-          <img src="images/logo.png" alt="a logo depicting a guitar player"></img>
           <h1 className="name name__first" key={generateID(8)}> Tyler</h1>
+          <img src="images/logo.png" alt="a logo depicting a guitar player"></img>
           <h1 className="name" key={generateID(8)}>Martin</h1>
         </div>
         <p className="bio">
