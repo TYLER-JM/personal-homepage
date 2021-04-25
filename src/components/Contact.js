6ef2d060cec37e890ec3751ab23b369405446b6b
Tyler Martin <tyler.martin@electric-playground.com>
2021-04-25 14:31:38 -0300

    updates contact card style and layout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,17 +6,19 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 const Contact = function() {
   return (
     <article className="section contact" id="contact">
-      <p>I'm always happy to chat about my code, the books I'm reading, games I'm playing, or music I'm listening to.</p>
-      <div>
-        <a className="social github" href="https://github.com/TYLER-JM" target="blank">
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
-        <a className="social linkedin" href="https://www.linkedin.com/in/tyler-martin-670020198/" target="blank">
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
-        <a className="social mail" href="mailto:tyler.j.a.martin@gmail.com" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faEnvelope} />
-        </a>
+      <div className="contact__container">
+        <p>I'm always happy to chat about my code, the books I'm reading, games I'm playing, or music I'm listening to.</p>
+        <div>
+          <a className="social github" href="https://github.com/TYLER-JM" target="blank">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a className="social linkedin" href="https://www.linkedin.com/in/tyler-martin-670020198/" target="blank">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a className="social mail" href="mailto:tyler.j.a.martin@gmail.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+        </div>
       </div>
     </article>
   )
